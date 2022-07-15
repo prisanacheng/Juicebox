70ffe041b70bae3d0427f5a5f26c39eb022268b5
Prisana Cheng <chengprisana@gmail.com>
2022-07-15 14:17:09 -0500

    successfully showing users

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,22 @@
-const { client, getAllUsers } = require('./index');
+const { client, getAllUsers, createUser } = require('./index');
+
+async function createInitialUsers (){
+    try {
+        console.log("starting to create users...")
+        const albert = await createUser({username: 'albert', password: 'bertie99'})
+        const sandra = await createUser({username: 'sandra', password: '2sandy4me'})
+        const glamgal = await createUser({username: 'glamgal', password: 'soglam'})
+        console.log(albert)
+        console.log(sandra)
+        console.log(glamgal)
+       
+
+        console.log("finished creating users")
+    } catch(error){
+        console.error("error creating users")
+        throw error;
+    }
+}
 
 async function dropTables() {
     try {
@@ -38,6 +56,7 @@ async function rebuildDB(){
 
         await dropTables();
         await createTables();
+        await createInitialUsers();
     } catch(error) {
         throw error
     } 
